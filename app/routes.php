027fe8ee47cb7ba8e4f304397846af3a2bde9d1e
nazmul86 <nazmul.is.awesome@gmail.com>
2015-02-04 14:43:32 +0600

    =before adding space

--- app/routes.php
+++ app/routes.php
@@ -1,38 +1,5 @@
 <?php
 
-
-/*
-campaign,token,name,age,gender,mobile,thana,(district),educational_qualification,occupation,coupon,currently_used_product,sales,product:quantity,sender_number
-*/
-
-
-/*
-u1,123456789,nazmul,25,male,01710340450,0024,5,1,123456,2,y,1:2,3:4,01710450340
-*/
-
-/*
-http://localhost/campaign/public/newapi/01710340450/token/u1,123456789,nazmul,25,male,01710340450,0024,5,1,123456,2,y,1:2,3:4
-*/
-
-// valid
-/*
-http://localhost/campaign/public/newapi/token/01710340450/u1,nazmul,25,m,01710340450,1001,5,1,81896021,2,y,1:2,3:4
-*/
-
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-
-
 Route::get('newapi/{token}/{bp_mobile}/{message}','MessagesController@newcreate');
 
 Route::controller('messages','MessagesController');
